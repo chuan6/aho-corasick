0c9b3d32c762da7af8a17ac7c6c5fb9cad67e31f
chuan6 <chuan6.dev@gmail.com>
2016-10-25 14:33:21 +0800

    draft on functions for goto-graph construction and query

--- src/aho_corasick/core.clj
+++ src/aho_corasick/core.clj
@@ -1,6 +1,44 @@
 (ns aho-corasick.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def start-state 0)
+(def fail-state? nil?)
+
+(defn- gq
+  "graph query function used during goto graph construction"
+  [g state c]
+  (get-in g [:states state c]))
+
+(defn- gex
+  "graph path extending function used during goto graph construction"
+  [g state c state']
+  (cond-> (assoc-in g [:states state c] state')
+    (> state' (:max-state g)) (assoc :max-state state')))
+
+(defn- search-keyword [g state xs]
+  (if (empty? xs)
+    [state ()]
+    (let [state' (gq g state (first xs))]
+      (if (fail-state? state')
+        [state xs]
+        (recur g state' (rest xs))))))
+
+(defn enter-keyword [g kw]
+  (let [[state kw'] (search-keyword g start-state kw)
+        new-states (iterate inc (inc (:max-state g)))]
+    (reduce
+     (fn [curr-g [s ns k]]
+       (gex curr-g s k ns))
+     g (->> kw'
+            (interleave (cons state new-states) new-states)
+            (partition 3 3)
+            (take (count kw'))))))
+
+(defn goto-graph [keyword-set]
+  (let [init-graph {:max-state start-state :states {}}]
+    (reduce enter-keyword init-graph keyword-set)))
+
+(defn g-query [g state c]
+  (let [r (gq g state c)]
+    (if (and (= state start-state) (fail-state? r))
+      0
+      r)))
