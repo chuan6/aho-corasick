7351a04eba3101bdfca7c382ce6936ca62f7d756
chuan6 <chuan6.dev@gmail.com>
2016-10-25 15:48:37 +0800

    implement output function in algorithm 1.

--- src/aho_corasick/core.clj
+++ src/aho_corasick/core.clj
@@ -14,6 +14,11 @@
   (cond-> (assoc-in g [:states state c] state')
     (> state' (:max-state g)) (assoc :max-state state')))
 
+(defn- gout
+  "associate the given keyword with the given state"
+  [g state kw]
+  (update-in g [:output state] (fnil conj #{}) kw))
+
 (defn- search-keyword [g state xs]
   (if (empty? xs)
     [state ()]
@@ -23,15 +28,17 @@
         (recur g state' (rest xs))))))
 
 (defn enter-keyword [g kw]
-  (let [[state kw'] (search-keyword g start-state kw)
-        new-states (iterate inc (inc (:max-state g)))]
-    (reduce
-     (fn [curr-g [s ns k]]
-       (gex curr-g s k ns))
-     g (->> kw'
-            (interleave (cons state new-states) new-states)
-            (partition 3 3)
-            (take (count kw'))))))
+  (let [[s kw'] (search-keyword g start-state kw)]
+    (as-> (reduce
+           (fn [[g s] [s' k]]
+             [(gex g s k s') s'])
+           [g s]
+           (->> kw'
+                (interleave (rest (iterate inc (:max-state g))))
+                (partition 2 2)
+                (take (count kw'))))
+        [result-graph result-state]
+      (gout result-graph result-state kw))))
 
 (defn goto-graph [keyword-set]
   (let [init-graph {:max-state start-state :states {}}]
@@ -42,3 +49,6 @@
     (if (and (= state start-state) (fail-state? r))
       0
       r)))
+
+(defn output [g state]
+  (get-in g [:output state]))
