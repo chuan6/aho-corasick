5a7c287eb76ca5446b50bcc8def7b0cc02516f60
chuan6 <chuan6.dev@gmail.com>
2016-10-26 11:26:44 +0800

    draft implementation of failure function construction
    ...it is currently kind of a mess :(

--- src/aho_corasick/core.clj
+++ src/aho_corasick/core.clj
@@ -47,8 +47,55 @@
 (defn g-query [g state c]
   (let [r (gq g state c)]
     (if (and (= state start-state) (fail-state? r))
-      0
+      start-state
       r)))
 
 (defn output [g state]
   (get-in g [:output state]))
+
+(defn f [g state]
+  (get-in g [:failure state]))
+
+(defn- failure-transitions [g]
+  (let [gtab (get g :states)
+        d1ss (vals (gtab start-state))
+        ftab (reduce #(assoc %1 %2 0) {} d1ss)]
+    (loop [queue (into clojure.lang.PersistentQueue/EMPTY d1ss)
+           ftab ftab]
+      (if (empty? queue)
+        (assoc g :failure ftab)
+        (let [r (peek queue)
+              queue' (pop queue)
+              xs (keys (gtab r))
+
+              [q ft]
+              (loop [xs (keys (gtab r))
+                     q queue'
+                     ft ftab]
+                (if (empty? xs)
+                  [q ft]
+                  (let [x (first xs)
+                        s (g-query g r x)]
+                    (recur (rest xs)
+                           (conj q s)
+                           (assoc ft s
+                                  (->> r
+                                       (iterate ft)
+                                       rest
+                                       (map #(g-query g % x))
+                                       (drop-while fail-state?)
+                                       first))))))]
+          (recur q ft))))))
+
+(= (failure-transitions (goto-graph #{"he" "she" "his" "hers"}))
+   {:max-state 9,
+    :states
+    {0 {\h 1, \s 7},
+     1 {\e 2, \i 5},
+     2 {\r 3},
+     3 {\s 4},
+     5 {\s 6},
+     7 {\h 8},
+     8 {\e 9}},
+    :output {4 #{"hers"}, 6 #{"his"}, 9 #{"she"}, 2 #{"he"}},
+    :failure {1 0, 7 0, 2 0, 5 0, 8 1, 3 0, 6 7, 9 2, 4 7}})
