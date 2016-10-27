6d86e51e1c007a6ddd0f0ebdba2fa5f0ca6a2781
chuan6 <chuan6.dev@gmail.com>
2016-10-27 15:59:47 +0800

    first version pushed to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject aho-corasick "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple Aho-Corasick implementation in Clojure"
+  :url "https://github.com/chuan6/aho-corasick"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
